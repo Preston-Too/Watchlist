894a0aded667eec317fa5126da9882ba339602f2
Preston-Too <toopreston@gmail.com>
2020-11-26 15:33:33 +0300

    deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY=1c4372d43ad7a419ab244af60ba3998d
-export SECRET_KEY=Press123
+export MOVIE_API_KEY='1c4372d43ad7a419ab244af60ba3998d'
+export SECRET_KEY='Press123'
 
 python3.8 manage.py server
