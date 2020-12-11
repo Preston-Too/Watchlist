be2437466b40b8297980c244878eac535b9c7e95
Preston-Too <toopreston@gmail.com>
2020-12-11 13:40:00 +0300

    modified templates

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 export MOVIE_API_KEY='1c4372d43ad7a419ab244af60ba3998d'
 export SECRET_KEY='Press123'
+export MAIL_USERNAME='toopreston@gmail.com'
+export MAIL_PASSWORD='Kipkirui'
+
 
 python3.8 manage.py server
